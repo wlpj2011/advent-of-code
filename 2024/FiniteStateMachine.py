7ab708c44ab7c27f6ddb112b3d34ca7b6236c84f
William Johnson <wlpj2011@gmail.com>
2024-12-28 14:36:29 -0500

    Wrote enough to test a basic FSM and moved it to it's own file

---
+++ 2024/FiniteStateMachine.py
@@ -0,0 +1,64 @@
+from typing import Tuple
+
+class FSM_State():
+    # I could give each state a list of rule instad of holding a list of all rule for the FSM
+    def __init__(self, state: str):
+        # A FSM State is defined by a lower case ascii string of characters
+        self.state = state.lower()
+        return
+
+    def __eq__(self, other: object) -> bool:
+        # Two FSM States are equal if they have the same name
+        if not isinstance(other, FSM_State):
+            return NotImplemented
+        return self.state == other.state
+
+    def __repr__(self) -> str:
+        return f"FSM_State('{self.state}')"
+
+        
+class FSM_Rule():
+    def __init__(self, start_state: FSM_State, end_state: FSM_State, accepted_input: str):
+        # A FSM Rule is defined by a starting and ending state and the input that must be provided to traverse the edge.
+        self.start_state = start_state
+        self.end_state = end_state
+        self.accepted_input = accepted_input
+        return
+
+    def __eq__(self, other: object) -> bool:
+        # TWo FSM Rules are equal if they have the same states and accepted input
+        if not isinstance(other, FSM_Rule):
+            return NotImplemented
+        return (self.start_state == other.start_state) and (self.end_state == other.end_state) and (self.accepted_input == other.accepted_input)
+        
+class FiniteStateMachine():
+    def __init__(self, states: list[FSM_State], rules: list[FSM_Rule]):
+        self.states = states
+        self.rules = rules
+        self.current_state = FSM_State("begin")
+        self.input_str = ""
+        return
+
+    def add_input(self, input_str: str) -> None:
+        self.input_str += input_str
+        return
+    
+    def add_state(self, state: FSM_State) -> None:
+        self.states.append(state)
+        return
+    
+    def add_rule(self, rule: FSM_Rule) -> None:
+        self.rules.append(rule)
+        return
+
+    def advance_FSM(self) -> None:
+        (new_char, remaining_input) = split_input(self.input_str)
+        for rule in self.rules:
+            if (rule.start_state == self.current_state) and (new_char == rule.accepted_input):
+                self.current_state = rule.end_state
+                self.input_str = remaining_input
+                break
+            
+def split_input(input_str: str) -> Tuple[str, str]:
+    return input_str[0], input_str[1:]
+
--- 2024/day-03-b.py
+++ 2024/day-03-b.py
@@ -1,26 +1,6 @@
-class FSM_State():
-    def __init__(self, state):
-        self.state = state
+from FiniteStateMachine import *
 
-        
-class FSM_Rule():
-    def __init__(self, rule):
-        self.rule = rule
-
-        
-class FiniteStateMachine():
-    def __init__(self, states, rules):
-        self.states = states
-        self.rules = rules
-
-    def add_state(self, state):
-        self.states.append(state)
-
-    def add_rule(self, rule):
-        self.rules.append(rule)
-
-
-def process_input(filename):
+def process_input(filename: str) -> list[str]:
     lines = []
     with open(filename, 'r') as file:
         for line in file.readlines():
@@ -37,6 +17,31 @@ if __name__ == "__main__":
     corrupted_memory = ["xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"]
     #corrupted_memory = process_input('input-03.txt')
 
+
+    begin = FSM_State("begin")
+    end = FSM_State("end")
+    state0 = FSM_State("0")
+    state1 = FSM_State("1")
+    ruleb0 = FSM_Rule(begin, state0, "0")
+    ruleb1 = FSM_Rule(begin, state1, "1")
+    rule00 = FSM_Rule(state0, end, "0")
+    rule11 = FSM_Rule(state1, end, "1")
+    rule01 = FSM_Rule(state0, state1, "1")
+    rule10 = FSM_Rule(state1, state0, "0")
+    rules = [ruleb0, ruleb1, rule00, rule11, rule01, rule10]
+    states = [begin, end, state0, state1]
+
+    test_FSM = FiniteStateMachine(states, rules)
+    input_str = "00"
+    test_FSM.add_input(input_str)
+
+    for _ in range(10):
+        print(test_FSM.current_state)
+        test_FSM.advance_FSM()
+    
+
+
+    
     #total_sum = process_memory(corrupted_memory)
     total_sum = 0
     print(f"Total of mul's is {total_sum}")
