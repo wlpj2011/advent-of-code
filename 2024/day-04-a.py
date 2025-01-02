7092d763cf5b1b821ad48a417f80b005f4e3be5b
William Johnson <wlpj2011@gmail.com>
2025-01-02 16:27:42 -0500

    Finished Day4a

--- 2024/day-04-a.py
+++ 2024/day-04-a.py
@@ -6,12 +6,40 @@ def process_input(filename: str) -> list[str]:
     return lines
 
 def count_crossword(crossword: list[str]) -> int:
+    total_count = 0
+    for i in range(len(crossword)):
+        for j in range(len(crossword[0])):
+            if crossword[i][j] == "X":
+                if i >= 3 and j >= 3:
+                    if (crossword[i-1][j-1] == "M") and (crossword[i-2][j-2] == "A") and (crossword[i-3][j-3] == "S"):
+                        total_count+=1
+                if i >= 3:
+                    if (crossword[i-1][j] == "M") and (crossword[i-2][j] == "A") and (crossword[i-3][j] == "S"):
+                        total_count+=1
+                if j >= 3:
+                    if (crossword[i][j-1] == "M") and (crossword[i][j-2] == "A") and (crossword[i][j-3] == "S"):
+                        total_count+=1
+                if i+3 < len(crossword) and j>=3:
+                    if (crossword[i+1][j-1] == "M") and (crossword[i+2][j-2] == "A") and (crossword[i+3][j-3] == "S"):
+                        total_count+=1
+                if i+3 < len(crossword):
+                    if (crossword[i+1][j] == "M") and (crossword[i+2][j] == "A") and (crossword[i+3][j] == "S"):
+                        total_count+=1
+                if j+3 < len(crossword) and i>=3:
+                    if (crossword[i-1][j+1] == "M") and (crossword[i-2][j+2] == "A") and (crossword[i-3][j+3] == "S"):
+                        total_count+=1
+                if j+3 < len(crossword):
+                    if (crossword[i][j+1] == "M") and (crossword[i][j+2] == "A") and (crossword[i][j+3] == "S"):
+                        total_count+=1
+                if i+3 < len(crossword) and j+3 < len(crossword):
+                    if (crossword[i+1][j+1] == "M") and (crossword[i+2][j+2] == "A") and (crossword[i+3][j+3] == "S"):
+                        total_count+=1
 
-    return 0
+    return total_count
 
 if __name__ == "__main__":
-    crossword = ["MMMSXXMASM", "MSAMXMSMSA", "AMXSXMAAMM", "MSAMASMSMX", "XMASAMXAMM", "XXAMMXXAMA", "SMSMSASXSS", "SAXAMASAAA", "MAMMMXMMMM", "MXMXAXMASX"]
-    #crossword = process_input('input-04.txt')
+    #crossword = ["MMMSXXMASM", "MSAMXMSMSA", "AMXSXMAAMM", "MSAMASMSMX", "XMASAMXAMM", "XXAMMXXAMA", "SMSMSASXSS", "SAXAMASAAA", "MAMMMXMMMM", "MXMXAXMASX"]
+    crossword = process_input('input-04.txt')
 
     total = count_crossword(crossword)
     print(f"Total number of XMAS's is {total}")
