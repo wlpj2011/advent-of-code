b7b3f1ce295db8be2a7e9ef9b36bd18e9f395e9e
William Johnson <wlpj2011@gmail.com>
2025-01-03 18:53:26 -0500

    created outline of solution for day5a

--- 2024/day-05-a.py
+++ 2024/day-05-a.py
@@ -16,13 +16,21 @@ def process_input(filename: str) -> Tuple[list[Tuple[int, int]], list[list[int]]
                 updates.append(int_update)
     return rules, updates
 
+def verify_update(update: list[int], rules: list[Tuple[int, int]]) -> bool:
+    
+    return False
+
+def verify_updates(updates: list[list[int]], rules: list[Tuple[int, int]]) -> int:
+    total = 0
+    for update in updates:
+        if verify_update(update, rules):
+            total += update[int((len(update) - 1)/2)]
+    return total
 
 if __name__ == "__main__":
     rules = [(47, 53), (97, 13), (97, 61), (97, 47), (75, 29), (61, 13), (75, 53), (29, 13), (97, 29), (53, 29), (61, 53), (97, 53), (61, 29), (47, 13), (75, 47), (97, 75), (47, 61), (75, 61), (47, 29), (75, 13), (53, 13)]
     updates = [[75, 47, 61, 53, 29], [97, 61, 53, 29, 13], [75, 29, 13], [75, 97, 47, 61, 53], [61, 13, 29], [97, 13, 75, 29, 47]]
     #rules, updates = process_input('input-05.txt')
 
-    #total = count_crossword(crossword)
-    print(len(rules), len(updates))
-    total = 0
+    total = verify_updates(updates, rules)
     print(f"Total sum of the middle of correct updates is {total}")
