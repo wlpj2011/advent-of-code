f9dc9f451645268acf86212b2891a84034dcd235
William Johnson <wlpj2011@gmail.com>
2025-01-05 14:45:43 -0500

    Finished Problem 5b

--- 2024/day-05-b.py
+++ 2024/day-05-b.py
@@ -36,10 +36,43 @@ def remove_valid_updates(updates: list[list[int]], rules: list[Tuple[int, int]])
             invalid_updates.append(update)
     return invalid_updates
 
+def modify_update(update: list[int], rules: list[Tuple[int, int]]) -> bool:
+    for page in update:
+        for rule in rules:
+            if page == rule[0]:
+                if rule[1] in update:
+                    if update.index(rule[1]) < update.index(page):
+                        new_index = update.index(page)
+                        old_index = update.index(rule[1])
+                        update[new_index] = rule[1]
+                        update[old_index] = page
+                        return False
+            if page == rule[1]:
+                if rule[0] in update:
+                    if update.index(rule[0]) > update.index(page):
+                        new_index = update.index(page)
+                        old_index = update.index(rule[0])
+                        update[new_index] = rule[0]
+                        update[old_index] = page
+                        return False
+    return True
+
+def fix_update(update: list[int], rules: list[Tuple[int, int]]):
+    while not modify_update(update, rules):
+        continue
+
+def fix_updates(updates: list[list[int]], rules: list[Tuple[int, int]]) -> int:
+    count = 0
+    for update in updates:
+        fix_update(update, rules)
+        count += update[int((len(update) - 1)/2)]
+    return count
+
 if __name__ == "__main__":
-    rules = [(47, 53), (97, 13), (97, 61), (97, 47), (75, 29), (61, 13), (75, 53), (29, 13), (97, 29), (53, 29), (61, 53), (97, 53), (61, 29), (47, 13), (75, 47), (97, 75), (47, 61), (75, 61), (47, 29), (75, 13), (53, 13)]
-    updates = [[75, 47, 61, 53, 29], [97, 61, 53, 29, 13], [75, 29, 13], [75, 97, 47, 61, 53], [61, 13, 29], [97, 13, 75, 29, 47]]
-    #rules, updates = process_input('input-05.txt')
+    #rules = [(47, 53), (97, 13), (97, 61), (97, 47), (75, 29), (61, 13), (75, 53), (29, 13), (97, 29), (53, 29), (61, 53), (97, 53), (61, 29), (47, 13), (75, 47), (97, 75), (47, 61), (75, 61), (47, 29), (75, 13), (53, 13)]
+    #updates = [[75, 47, 61, 53, 29], [97, 61, 53, 29, 13], [75, 29, 13], [75, 97, 47, 61, 53], [61, 13, 29], [97, 13, 75, 29, 47]]
+    rules, updates = process_input('input-05.txt')
 
     invalid_updates = remove_valid_updates(updates, rules)
-    print(f"Total sum of the middle of correct updates is {invalid_updates}")
+    count = fix_updates(invalid_updates, rules)
+    print(f"Total sum of the middle of correct updates is {count}")
