05a824384157b55d9106c02b07b4ea8ea61c7674
William Johnson <wlpj2011@gmail.com>
2025-01-06 17:15:02 -0500

    Started work on day6a

--- 2024/day-06-a.py
+++ 2024/day-06-a.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 def process_input(filename: str) -> list[str]:
     lines = []
     with open(filename, 'r') as file:
@@ -6,8 +8,21 @@ def process_input(filename: str) -> list[str]:
     return lines
 
 def count_guard_squares(guard_map: list[str]) -> int:
-
     return 0
+
+def locate_guard(guard_map: list[str]) -> Tuple[int, int, int]:
+    for i in range(len(guard_map)):
+        for j in range(len(guard_map[i])):
+            if guard_map[i][j] == "^":
+                new_guard_map_row = list(guard_map[i])
+                new_guard_map_row[j] = "."
+                guard_map[i] = str(new_guard_map_row)
+                return (i, j, 0)
+    return (0,0,0)
+            
+def move_guard(guard_map: list[str], guard_location: Tuple[int, int, int]) -> Tuple[int, int, int]:
+    
+    return guard_location
 
 if __name__ == "__main__":
     guard_map = ["....#.....", ".........#", "..........", "..#.......", ".......#..", "..........", ".#..^.....", "........#.", "#.........", "......#..."]
