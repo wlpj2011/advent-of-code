196b8075aa26acf9cf093eefdfc974f8e3c58b94
William Johnson <wlpj2011@gmail.com>
2025-10-29 15:36:38 -0400

    2023-09b: Finished part b of day 9

--- 2023/day09/src/main.rs
+++ 2023/day09/src/main.rs
@@ -1,6 +1,7 @@
 use anyhow::Result;
 use clap::Parser;
 use itertools::Itertools;
+use std::collections::VecDeque;
 use std::fs::File;
 use std::io::BufReader;
 use std::io::prelude::*;
@@ -30,17 +31,17 @@ struct Group {
 
 #[derive(Debug, Clone)]
 struct History {
-    vals: Vec<i64>,
-    diffs: Vec<Vec<i64>>,
+    vals: VecDeque<i64>,
+    diffs: Vec<VecDeque<i64>>,
     kth_diff: Option<i64>,
     k: Option<u64>,
 }
 
 impl History {
     fn from_string(string: String) -> Result<History> {
-        let mut vals: Vec<_> = Vec::new();
+        let mut vals: VecDeque<_> = VecDeque::new();
         for num in string.split(" ").collect::<Vec<_>>() {
-            vals.push(num.trim().parse::<i64>()?)
+            vals.push_front(num.trim().parse::<i64>()?)
         }
         Ok(History {
             vals,
@@ -53,21 +54,21 @@ impl History {
     fn find_diff(&mut self) {
         loop {
             if self.diffs.is_empty() {
-                let mut next_diffs: Vec<_> = Vec::new();
+                let mut next_diffs: VecDeque<_> = VecDeque::new();
                 for (first, second) in self.vals.iter().tuple_windows() {
-                    next_diffs.push(second - first);
+                    next_diffs.push_back(first - second);
                 }
                 self.diffs.push(next_diffs);
             } else {
-                let mut next_diffs: Vec<_> = Vec::new();
+                let mut next_diffs: VecDeque<_> = VecDeque::new();
                 for (first, second) in self.diffs.last().unwrap().iter().tuple_windows() {
-                    next_diffs.push(second - first);
+                    next_diffs.push_back(first - second);
                 }
                 self.diffs.push(next_diffs);
             }
             if self.diffs.last().unwrap().iter().all(|x| *x == 0) {
                 self.k = Some((self.diffs.len() - 1) as u64);
-                self.kth_diff = Some(*self.diffs[(self.k.unwrap() - 1) as usize].last().unwrap());
+                self.kth_diff = Some(*self.diffs[(self.k.unwrap() - 1) as usize].back().unwrap());
                 break;
             }
         }
@@ -75,11 +76,21 @@ impl History {
 
     fn compute_next(&mut self) -> i64 {
         for i in (0..(self.diffs.len() - 1)).rev() {
-            let next_diff = *self.diffs[i].last().unwrap() + self.diffs[i + 1].last().unwrap();
-            self.diffs[i].push(next_diff);
+            let next_diff = *self.diffs[i].front().unwrap() + self.diffs[i + 1].front().unwrap();
+            self.diffs[i].push_front(next_diff);
         }
-        let new_val = (*self.diffs.first().unwrap()).last().unwrap() + self.vals.last().unwrap();
-        self.vals.push(new_val);
+        let new_val = (*self.diffs.first().unwrap()).front().unwrap() + self.vals.front().unwrap();
+        self.vals.push_front(new_val);
+        new_val
+    }
+
+    fn compute_past(&mut self) -> i64 {
+        for i in (0..(self.diffs.len() - 1)).rev() {
+            let next_diff = *self.diffs[i].back().unwrap() - self.diffs[i + 1].back().unwrap();
+            self.diffs[i].push_back(next_diff);
+        }
+        let new_val = -(*self.diffs.first().unwrap()).back().unwrap() + self.vals.back().unwrap();
+        self.vals.push_back(new_val);
         new_val
     }
 }
@@ -105,6 +116,9 @@ fn solution_b(file: File) -> Result<i64> {
     let mut result = 0;
 
     while reader.read_line(&mut line)? != 0 {
+        let mut hist = History::from_string(line.clone())?;
+        hist.find_diff();
+        result += hist.compute_past();
         line.clear();
     }
 
@@ -124,7 +138,7 @@ fn main() -> Result<()> {
         let file = File::open(args.file.clone())?;
         let result = solution_b(file)?;
 
-        println!("The sum of the next values for each history is {result}.");
+        println!("The sum of the extrapolated past values for each history is {result}.");
     }
 
     Ok(())
@@ -143,7 +157,7 @@ mod tests {
 
     #[test]
     fn test_solution_b() -> Result<()> {
-        assert_eq!(solution_b(File::open("test-input-09.txt")?)?, 0);
+        assert_eq!(solution_b(File::open("test-input-09.txt")?)?, 2);
         Ok(())
     }
 }
